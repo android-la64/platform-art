593bb78d69834282cb88a531ae9ce08d6c7aebf2
Qizheng Zhu <zhuqizheng@loongson.cn>
2025-02-20 11:01:35 +0800

    [nterp] ExecuteNterpImpl with class init(ExecuteNterpWithClinitImpl).

--- runtime/nterp_helpers.cc
+++ runtime/nterp_helpers.cc
@@ -243,17 +243,14 @@ bool CanMethodUseNterp(ArtMethod* method, InstructionSet isa) {
     return false;  // Riscv64 and LoongArch64 nterp does not support compact dex yet.
   }
   if (isa == InstructionSet::kLoongarch64) {
-    if (method->NeedsClinitCheckBeforeCall()) {
-      return false;  // Riscv64 nterp does not implement ExecuteNterpWithClinitImpl.
-    }
     if (method->GetDexFile()->IsCompactDexFile()) {
-      return false;  // Riscv64 nterp does not support compact dex yet.
+      return false;  // Loongarch64 nterp does not support compact dex yet.
     }
     if (method->DexInstructionData().TriesSize() != 0u) {
-      return false;  // Riscv64 nterp does not support exception handling yet.
+      return false;  // Loongarch64 nterp does not support exception handling yet.
     }
     if (method->DexInstructionData().InsSize() != 0u) {
-      return false;  // Riscv64 nterp does not support argument processing yet.
+      return false;  // Loongarch64 nterp does not support argument processing yet.
     }
     for (DexInstructionPcPair pair : method->DexInstructions()) {
       // TODO(loongarch64): Add support for more instructions.
