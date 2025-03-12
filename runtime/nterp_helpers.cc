a001ded51913cf8cc91d3d2049d6da5e4e1d7520
Qizheng Zhu <zhuqizheng@loongson.cn>
2025-03-12 17:06:05 +0800

    [nterp] Add AGET/APUT/CONST opcodes.

--- runtime/nterp_helpers.cc
+++ runtime/nterp_helpers.cc
@@ -280,6 +280,9 @@ bool CanMethodUseNterp(ArtMethod* method, InstructionSet isa) {
         case Instruction::CONST_WIDE_32:
         case Instruction::CONST_WIDE:
         case Instruction::CONST_WIDE_HIGH16:
+        case Instruction::CONST_STRING:
+        case Instruction::CONST_STRING_JUMBO:
+        case Instruction::CONST_CLASS:
         case Instruction::SGET:
         case Instruction::SGET_WIDE:
         case Instruction::SGET_OBJECT:
@@ -294,6 +297,20 @@ bool CanMethodUseNterp(ArtMethod* method, InstructionSet isa) {
         case Instruction::FILLED_NEW_ARRAY_RANGE:
         case Instruction::FILL_ARRAY_DATA:
         case Instruction::THROW:
+        case Instruction::AGET:
+        case Instruction::AGET_WIDE:
+        case Instruction::AGET_OBJECT:
+        case Instruction::AGET_BOOLEAN:
+        case Instruction::AGET_BYTE:
+        case Instruction::AGET_CHAR:
+        case Instruction::AGET_SHORT:
+        case Instruction::APUT:
+        case Instruction::APUT_WIDE:
+        case Instruction::APUT_OBJECT:
+        case Instruction::APUT_BOOLEAN:
+        case Instruction::APUT_BYTE:
+        case Instruction::APUT_CHAR:
+        case Instruction::APUT_SHORT:
         case Instruction::IGET:
         case Instruction::IGET_WIDE:
         case Instruction::IGET_OBJECT:
@@ -413,6 +430,8 @@ bool CanMethodUseNterp(ArtMethod* method, InstructionSet isa) {
         case Instruction::INVOKE_POLYMORPHIC_RANGE:
         case Instruction::INVOKE_CUSTOM:
         case Instruction::INVOKE_CUSTOM_RANGE:
+        case Instruction::CONST_METHOD_HANDLE:
+        case Instruction::CONST_METHOD_TYPE:
           continue;
         default:
           return false;
