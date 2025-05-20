20c0abfc20aa72dd4f77b973c1c9a5d6b0674de4
Yanxi <yanxi@loongson.cn>
2025-05-20 10:10:22 +0800

    LoongArch64: Sync GetCriticalNativeStubFrameSize fix from AOSP 15
    - Based on Android 12 patch (13f071422fcd1dffdb0bcc3a9f1e6c774e52ded0).
    - Reference changes from AOSP 15 commit 0f6befa0f064d482cc252913f46ba8264b7e0384 to update GetCriticalNativeStubFrameSize for LoongArch64.
    - Ensured compatibility and alignment with recent changes in other architectures.
    - Verified all LoongArch64 tests pass (m art_compiler_host_tests).

--- runtime/entrypoints/jni/jni_entrypoints.cc
+++ runtime/entrypoints/jni/jni_entrypoints.cc
@@ -22,6 +22,7 @@
 #include "arch/riscv64/jni_frame_riscv64.h"
 #include "arch/x86/jni_frame_x86.h"
 #include "arch/x86_64/jni_frame_x86_64.h"
+#include "arch/loongarch64/jni_frame_loongarch64.h"
 #include "art_method-inl.h"
 #include "dex/dex_instruction-inl.h"
 #include "dex/method_reference.h"
@@ -153,6 +154,8 @@ extern "C" size_t artCriticalNativeFrameSize(ArtMethod* method, uintptr_t caller
         return x86::GetCriticalNativeStubFrameSize(shorty);
       case InstructionSet::kX86_64:
         return x86_64::GetCriticalNativeStubFrameSize(shorty);
+      case InstructionSet::kLoongarch64:
+        return loongarch64::GetCriticalNativeStubFrameSize(shorty);
       default:
         UNIMPLEMENTED(FATAL) << kRuntimeISA;
         UNREACHABLE();
@@ -190,6 +193,8 @@ extern "C" size_t artCriticalNativeFrameSize(ArtMethod* method, uintptr_t caller
         return x86::GetCriticalNativeDirectCallFrameSize(shorty);
       case InstructionSet::kX86_64:
         return x86_64::GetCriticalNativeDirectCallFrameSize(shorty);
+      case InstructionSet::kLoongarch64:
+        return loongarch64::GetCriticalNativeDirectCallFrameSize(shorty);
       default:
         UNIMPLEMENTED(FATAL) << kRuntimeISA;
         UNREACHABLE();
